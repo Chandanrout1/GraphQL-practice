c1a3a9214dc0c527b24aaa359ae452887f721f4c
Pravu chandan Rout <140379711+Chandanrout1@users.noreply.github.com>
2024-10-14 23:27:43 +0530

    complete

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { gql, useQuery } from "@apollo/client";
+
+const query = gql`
+  query getAllTodos {
+    getTodos {
+      id
+      title
+      completed
+      user {
+        id
+        name
+        phone
+      }
+    }
+  }
+`;
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { data, loading } = useQuery(query);
 
+  if (loading) return <h1>Loading data...</h1>;
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div>GraphQL</div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        {/* {JSON.stringify(data)} */}
+        <table>
+          <tbody>
+            {data.getTodos.map(todo => <tr key={todo.id}>
+              <td>{todo?.title}</td>
+              <td>{todo?.user?.name}</td>
+            </tr>)}
+          </tbody>
+        </table>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
